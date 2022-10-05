79aad238b7097f2ffc949037b4a616aa2db1c3a3
liuming13 <lxm210787>
2022-10-05 20:44:21 +0800

    first commit

---
+++ SparkDemo/src/main/java/org/llm/spark/chapter02/Test.scala
@@ -0,0 +1,16 @@
+package org.llm.spark.chapter02
+
+/**
+ * @description: TODO 
+ * @author ming
+ * @email 2107873642@qq.com
+ * @date 2022/10/5 20:43
+ * @version 1.0
+ */
+object Test {
+
+  def main(args: Array[String]): Unit = {
+    println("hello")
+  }
+
+}
